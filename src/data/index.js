671e8dcbf2d14900236bd3b7bd06be8abbdbf6e1
Tolu brymoh <83027769+brymoh7@users.noreply.github.com>
2024-03-16 00:15:12 +0100

    Add files via upload
    initial commit

---
+++ src/data/index.js
@@ -0,0 +1,84 @@
+const employeesData = [
+    {
+      id: 1,
+      firstName: 'Chioma',
+      lastName: 'Malife',
+      email: 'promise@example.com',
+      salary: '200000',
+      date: '2019-04-20'
+    },
+    {
+      id: 2,
+      firstName: 'Adrienne',
+      lastName: 'clark',
+      email: 'adrienne@example.com',
+      salary: '80000',
+      date: '2019-04-18'
+    },
+    {
+      id: 3,
+      firstName: 'Amara',
+      lastName: 'Hedva',
+      email: 'amara@example.com',
+      salary: '79000',
+      date: '2019-05-01'
+    },
+    {
+      id: 4,
+      firstName: 'Timi',
+      lastName: 'Philip',
+      email: 'philip@example.com',
+      salary: '56000',
+      date: '2019-05-03'
+    },
+    {
+      id: 5,
+      firstName: 'Jose',
+      lastName: 'Grant',
+      email: 'jose@example.com',
+      salary: '65000',
+      date: '2019-06-13'
+    },
+    {
+      id: 6,
+      firstName: 'Laurena',
+      lastName: 'Lurie',
+      email: 'laurena@example.com',
+      salary: '120000',
+      date: '2019-07-30'
+    },
+    {
+      id: 7,
+      firstName: 'George',
+      lastName: 'Adebayo',
+      email: 'george@example.com',
+      salary: '90000',
+      date: '2019-08-15'
+    },
+    {
+      id: 8,
+      firstName: 'Jesica',
+      lastName: 'Watlington',
+      email: 'jesica@example.com',
+      salary: '60000',
+      date: '2019-10-10'
+    },
+    {
+      id: 9,
+      firstName: 'Kelechi',
+      lastName: 'Warren',
+      email: 'kelechi@example.com',
+      salary: '71000',
+      date: '2019-10-15'
+    },
+    {
+      id: 10,
+      firstName: 'Lyndsey',
+      lastName: 'David',
+      email: 'lyndsey@example.com',
+      salary: '110000',
+      date: '2020-01-15'
+    }
+  ];
+  
+  export { employeesData };
